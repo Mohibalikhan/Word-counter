d8c6d9a5d31e6576c859a13b47dcc62ca1e735b4
Mohibalikhan <khanmohibali1@gmail.com>
2024-06-13 15:19:25 +0500

    Characters and Words counter

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please Enter your Sentence ="
+});
+let word_count = user_ans.words.trim().split(" ").length;
+let characters_count = user_ans.words.replace(/\s/g, '').length;
+console.log(`=> Your Sentence has "${word_count}" words.`);
+console.log(`=> And "${characters_count}" characters(without whitespaces).`);
